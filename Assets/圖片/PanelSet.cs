6eae192ea8d0a2890e12ae6ec7fce5fdeae2b495
sasalicat <qq695729652@gmail.com>
2019-08-11 17:18:01 +0800

    初始版本

---
+++ Assets/圖片/PanelSet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelSet : MonoBehaviour {
+    public GameObject[] panels;
+    public void SwitchPanel(int index)
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            if (i != index)
+            {
+                panels[i].SetActive(false);
+            }
+            else {
+                panels[i].SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/圖片/randIntPanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class randIntPanel : MonoBehaviour {
+    public Text numTxt;//手動拉取
+    public void onClick()
+    {
+        int num= Random.Range(0, 101);
+        numTxt.text = "" + num;
+    }
+}
