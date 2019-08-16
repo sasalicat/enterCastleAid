b1878d20aad3d7ff9057155c4c8f16f3ddf1cbe2
sasalicat <waimaoisme@126.com>
2019-08-16 21:06:04 +0800

    完成了玩家信息

---
+++ Assets/玩家面板/playerPanel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerPanel : MonoBehaviour {
+
+    public Text moneyText;
+    public Text levelText;
+    protected int money;
+    protected int level;
+    public int Money
+    {
+        set
+        {
+            if (value >= 0)
+            {
+                money = value;
+            }
+            else
+            {
+                money = 0;
+            }
+            moneyText.text = money + "";
+        }
+        get {
+            return money;
+        }
+    }
+    public int Level
+    {
+        set
+        {
+            if (value >= 1)
+            {
+                level = value;
+            }
+            else
+            {
+                level = 1;
+            }
+            levelText.text = level + "";
+        }
+        get
+        {
+            return level;
+        }
+    }
+    public void changeMoney(int offset)
+    {
+        Money += offset;
+    }
+    public void changeLevel(int offset)
+    {
+        Level += offset;
+    }
+}
